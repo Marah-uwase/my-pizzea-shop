19c082f856e2140398a6d5b2cb3b28797388caab
Marah-uwase <maranahuwase12@gmail.com>
2020-10-12 22:27:32 +0200

    check price with flavour

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-
+$(function () {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+  //Get inputs
+    $("#checkout").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
+  
+        //Function order
+        let order = (f, s, c, t, n, total) => {
+            return {f, s, c, t, n, total};
+        };
+  
       //check price
       let price, totalPrice;
       switch (flavour) {
